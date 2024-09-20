35b567aa0481063cd287dd72052ac7e2d8ab66df
liisa kaju <liisa.kaju@gmail.com>
2024-09-20 13:00:41 +0300

    Initial commit. added js and hmtl

---
+++ basics.js
@@ -0,0 +1,41 @@
+// Global Scope
+const teachername = "Karl"
+// DATA TYPES
+// String "See on string väärtus", 'See on samuti string väärtus', ´See on ka string väärtus`
+// Number (Int, float) => 42
+// Boolean => true/false, jah/ei, 1/0
+// const isMate = false
+// Array => []
+// Object => {}
+   
+
+// Muutujad - nimetus ja väärtus (NP: Muutumatu vs Muudetav)
+//const fruit = "Banaan"; Muutuja mida ei saa muuta
+// Muutujate nimetus on camelCase and it makes more sense
+//let model = "BMW"; Muutuja mida saab muuta, nimi ja väärtus
+
+model = "Audi";
+// snakeCase
+
+// Koodiblokk= scope
+// Koodiblokki tähistab {} sümbol (objekti sümbol)
+// Function Scope
+//const calculateSum = (number1, number2) => 
+
+
+// Type casting
+// Muudame number andmetüübi
+const castNumber = 123456;
+console.log(typeof(String(castNumber));
+console.log(Boolean(String(castNumber));
+
+//Array => sümbol on []
+const characters = ["T", "P", "I", "C", "S", "V", "A", "J", "R"];
+console.log(characters[7]);
+console.log(characters[6]);
+console.log(characters[5]);
+console.log(characters[4]);
+console.log(characters[3]);
+console.log(characters[8]);
+console.log(characters[2]);
+git comm
